eb7cfc68e485716e265c59fdb62f8e0301a1fcf6
marianac_md <marianacostamedeiros1@gmail.com>
2021-09-13 21:51:02 -0300

    DevStore-Site

---
+++ react/dev/src/components/cabecalho/index.js
@@ -0,0 +1,17 @@
+export default function Cabecalho() { 
+
+    return(
+        <div className="Cabecalho"> 
+            <div className="Area1"> 
+                <div className="Perfil-photo"> </div>
+                <div className="Perfil-name"> </div>
+            </div>
+
+            <div className="Icons"> 
+                <div className="Reloading"> </div>
+                <div className="Logo-out">  </div>
+        </div>
+    </div>
+
+    )
+}
---
+++ react/dev/src/components/menu/index.js
@@ -0,0 +1,16 @@
+export default function Menu() { 
+
+    return (
+        <div className = "Menu">
+            <div className="Marca"> 
+                <div className="Logo"> </div>
+                <div className="Titulo"> <span>Dev</span>Store</div>
+            </div>
+
+            <div className="CampoVazio"> </div>
+            <div className="Gerenciar"> Gerenciamento </div>
+            <div className="Produtos"> Produtos </div>
+            <div className="CampoVazio"> </div>
+        </div>
+    )
+}
